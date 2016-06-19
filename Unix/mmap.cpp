17933c94fb33949742f23868a758268c737d2f1c
Xendox <xendoxer@gmail.com>
2016-06-19 02:07:52 +0000

    Fix #1 Code repeat

--- Unix/mmap.cpp
+++ Unix/mmap.cpp
@@ -1,7 +1,23 @@
-#include "apue.h"
+#include "/home/xendox/apue.h"
+#include <stdarg.h>
 #include <fcntl.h> 
 #include <sys/mman.h>
 #include <iostream>
+
+void error_mess(const char*mess, ...)
+{
+  va_list va;
+  va_start(va,mess);
+  for(const char*p=mess;*p;++p)
+  {
+    if(*p=='%' && *++p=='s')
+      std::cout << va_arg(va,char*) << " ";
+    else std::cout << *p;
+  }
+  std::cout << std::endl;
+  va_end(va);
+  exit(-1);
+}
  
 int main(int argc, char *argv[]) 
 { 
@@ -9,45 +25,21 @@ int main(int argc, char *argv[])
   void *src, *dst; 
   struct stat statbuf; 
   if (argc != 3) 
-  {
-  	std::cout << "Arguments mistake" << std::endl;
-	exit(-1);
-  }
+  	error_mess("Arguments mistake");
   if ( (fdin = open(argv[1], O_RDONLY)) < 0 ) 
-  {
-    	std::cout << "невозможно открыть " << argv[1] << " для чтения" << std::endl;
-	exit(-1);
-  } 
+    	error_mess("Невозможно открыть %s для чтения",argv[1]);
   if ( (fdout = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, FILE_MODE)) < 0 )
-  { 
-    	std::cout << "невозможно создать" << argv[2] << " для записи" << std::endl;
-	exit(-1);
-  } 
+  	error_mess("Невозможно создать %s для записи", argv[2]);
   if ( fstat(fdin, &statbuf) < 0 )
-  { 
-    	std::cout << "fstat error" << std::endl;
-	exit(-1);
-  } 
-  if ( lseek(fdout, statbuf.st_size - 1, SEEK_SET) == -1 ) 
-  {
-	std::cout << "ошибка вызова функции lseek" << std::endl;
-	exit(-1);
-  } 
+  	error_mess("fstat error");
+  if ( lseek(fdout, statbuf.st_size - 1, SEEK_SET) == -1 )
+	error_mess("Ошибка вызова функции lseek");
   if ( write(fdout, "", 1) != 1 )
-  { 
-  	std::cout << "ошибка вызова функции write" << std::endl;
-	exit(-1);
-  } 
+  	error_mess("Ошибка вызова функции write");
   if ( (src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0)) == MAP_FAILED ) 
-  {
-  	std::cout << "ошибка вызова функции mmap для входного файла" << std::endl;
-	exit(-1);
-  } 
+  	error_mess("Ошибка вызова функции mmap для входного файла");
   if ( (dst = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fdout, 0)) == MAP_FAILED ) 
-  {
-  	std::cout << "ошибка вызова функции mmap для выходного файла" << std::endl;
-	exit(-1);
-  } 
+  	error_mess("Ошибка вызова функции mmap для выходного файла");
   memcpy(dst, src, statbuf.st_size); 
   return 0; 
 }
