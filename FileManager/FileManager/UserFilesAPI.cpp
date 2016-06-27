a722bcfcec70a6c8b5e56afe348f1f84d9fc8c86
Artem <artemtrofimushkin@gmail.com>
2016-06-28 00:36:45 +0300

    Change structure; Minor fix

---
+++ FileManager/FileManager/UserFilesAPI.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include "UserFilesAPI.h"
+
+data_map::data_map()
+{
+	
+}
+
+data_map::~data_map()
+{
+	
+}
+
+data_map::files_t data_map::user_files (const std::string & login)
+{
+	files_t files;
+	for (auto &r : data_[login].files()) {
+		files.push_back(r.first);
+	}
+	return files;
+}
