19b4aedfcefd13ef195d099842b0ce856df00e48
Evan Khmelevskiy <ivan.khm@gmai.com>
2016-06-29 14:50:40 +0300

    Unix get_ip added

--- FileManager/FileManager/GetIp.cpp
+++ FileManager/FileManager/GetIp.cpp
@@ -9,9 +9,7 @@ namespace ftp {
 	unsigned long get_ip() {
 		char buf[80];
 		::gethostname(buf, 80);
-		
 		hostent * phostent = ::gethostbyname(buf);
-		
 		if (phostent && phostent->h_addr_list) {
 			in_addr addr;
 			::memcpy(&addr, phostent->h_addr_list[0], sizeof(struct in_addr));
@@ -21,10 +19,18 @@ namespace ftp {
 	}
 }
 #elif defined(__unix__) || defined(__APPLE__)
-#incluede "UnixAPI.h"
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 unsigned long get_ip() {
-
-	return 0;
+	struct ifaddrs * ifAddrStruct = NULL;
+	unsigned long ip;
+	getifaddrs(&ifAddrStruct);
+	if (!ifAddrStruct) return -1;
+	ip = ((struct sockaddr_in *)ifAddrStruct[9].ifa_addr)->sin_addr.s_addr;//под индексом 9 лежит наш ip адрес. хз почему именно так, но оно работает.
+	freeifaddrs(ifAddrStruct);
+	return ip;
 }
 #endif
